5f312b44196c59510237e3bd9418fe4b4e26b949
Gabriel Benson <me@gabe.xyz>
2017-05-04 15:19:22 -0400

    basically done

--- index.js
+++ index.js
@@ -4,99 +4,136 @@ const nunjucks = require('nunjucks');
 const stylus = require('stylus');
 const path = require('path');
 const request = require('request');
-// const async = require('async');
-// const fs = require('fs');
+const async = require('async');
+const fs = require('fs');
 const app = express();
-// const apiKeyPath = path.join(__dirname, '/api.key');
-// const tmdbJSON = path.join(__dirname, '/moviedb.json');
+const apiKeyPath = path.join(__dirname, '/api.key');
+const tmdbJSON = path.join(__dirname, '/moviedb.json');
+const rtURL = 'https://www.rottentomatoes.com/search/?search=';
 
-// Set up themoviedb
-// if (!fs.existsSync(apiKeyPath)) {
-// 	console.log('API key file not found.  Please save your TMDb api key in a file called "api.key" at the root of the project.');
-// 	process.exit(1);
-// }
-// const moviedb = require('moviedb')(fs.readFileSync(apiKeyPath).toString().replace(/\r?\n|\r/g, ''));
-// var tmdbConfig;
-// fs.readFile(tmdbJSON, (err, data) => {
-// 	if (!err) {
-// 		tmdbConfig = JSON.parse(data);
-// 	}
-// 	moviedb.configuration((err, config) => {
-// 		if (!err) {
-// 			console.log(new Error(err));
-// 		} else {
-// 			tmdbConfig = config;
-// 			fs.writeFile(tmdbJSON, data, (err) => {
-// 				if (err) console.log(new Error(err));
-// 			});
-// 		}
-// 	});
-// });
+// Set up moviedb
+if (!fs.existsSync(apiKeyPath)) {
+	console.log(new Error('API key file not found.  Please save your TMDb api key in a file called "api.key" at the root of the project.'));
+	process.exit(1);
+}
+const moviedb = require('moviedb')(fs.readFileSync(apiKeyPath).toString().replace(/\r?\n|\r/g, ''));
 
-// Turn on stylus autocompiling
-app.use(stylus.middleware({
-	src: path.join(__dirname, '/resources'),
-	dest: path.join(__dirname, '/public'),
-	force: false,
-	compress: true
-}));
+async.series([
+	(callback) => {
+		fs.readFile(tmdbJSON, (err, data) => {
+			if (err) {
+				callback(err, getTMDbConfig());
+			} else {
+				data = JSON.parse(data);
+				data.stale = true;
+				callback(null, data);
+			}
+		});
+	}
+], (err, tmdbConfig) => {
+	tmdbConfig = tmdbConfig[0];
+	// if (err) console.log(new Error(err));
 
-// Get nunjucks going
-nunjucks.configure('views', {
-	autoescape: true,
-	noCache: true,
-	express: app
-});
+	if (tmdbConfig && tmdbConfig.stale) tmdbConfig = getTMDbConfig() || tmdbConfig;
 
-// Turn on public folder
-app.use(express.static('public'));
+	// Turn on stylus autocompiling
+	app.use(stylus.middleware({
+		src: path.join(__dirname, '/resources'),
+		dest: path.join(__dirname, '/public'),
+		force: false,
+		compress: true
+	}));
 
-app.get('/', (req, res) => {
-	res.render('index.njk');
-});
+	// Get nunjucks going
+	nunjucks.configure('views', {
+		autoescape: true,
+		noCache: true,
+		express: app
+	});
 
-app.get(/\/film\/.*/, (req, res) => {
-	request('https://www.rottentomatoes.com/search/?search=' + req.url.substring(6), (err, response, html) => {
-		if (!err && response.statusCode === 200) {
-			const anchorString = req.url.substring(6).replace(/%20/g, ' ') + '\', ';
-			var searchData = html.substring(html.indexOf(anchorString) + anchorString.length);
-			searchData = searchData.substring(0, searchData.indexOf(');'));
-			searchData = JSON.parse(searchData).movies;
-			res.render('result-list.njk', { movies: searchData });
-			// async.each(searchData, (movie, callback) => {
-			// 	const movieURL = 'https://api.themoviedb.org/3/search/movie?api_key=' + apiKey + '&language=en-US&page=1&include_adult=false' + '&query=';
-			// 	request.get(movieURL + movie.name, (err, response, movieData) => {
-			// 		movieData = JSON.parse(movieData).results[0];
-			// 		if (!err && response.statusCode === 200) {
-			// 			if (movieData) {
-			// 				movie.image = moviedbConfig.images.secure_base_url + moviedbConfig.images.backdrop_sizes[0] + movieData.backdrop_path;
-			// 				movie.overview = movieData.overview;
-			// 			}
-			// 			callback();
-			// 		}
-			// 	});
-			// }, (err) => {
-			// 	if (err) console.log(new Error(err));
-			// 	res.render('result-list.njk', { movies: searchData });
-			// });
-		} else {
-			// Rotten Tomatoes failed, for whatever reason
-			if (err) console.log(new Error(err));
-			res.render('oops.njk');
-		}
+	// Turn on public folder
+	app.use(express.static('public'));
+
+	app.get('/', (req, res) => {
+		res.render('index.njk');
+	});
+
+	app.get('/film/:searchTerms', (req, res) => {
+		const searchTerms = req.params.searchTerms.replace(/%20/g, ' ');
+		async.parallel([
+			(callback) => {
+				request(rtURL + req.url.substring(6), (err, response, html) => {
+					if (!err && response.statusCode === 200) {
+						const anchorString = searchTerms + '\', ';
+						var searchData = html.substring(html.indexOf(anchorString) + anchorString.length);
+						searchData = searchData.substring(0, searchData.indexOf(');'));
+						callback(null, JSON.parse(searchData).movies);
+					} else {
+						callback(err || 'Invalid response from RT: ' + response.statusCode);
+					}
+				});
+			},
+			(callback) => {
+				moviedb.searchMovie({ query: searchTerms }, (err, data) => {
+					if (!err) {
+						console.log(JSON.stringify(data.results, null, 2));
+						callback(null, data.results.sort((a, b) => {
+							if (a.popularity < b.popularity) return -1;
+							if (a.popularity > b.popularity) return 1;
+							return 0;
+						}));
+					} else {
+						callback(err);
+					}
+				});
+			}
+		], (err, results) => {
+			if (!err) {
+				async.each(results[0], (rtMovie, outerCallback) => {
+					async.each(results[1], (dbMovie, innerCallback) => {
+						if (dbMovie.release_date.indexOf(rtMovie.year) !== -1) {
+						// if (dbMovie.title === rtMovie.name || dbMovie.release_date.indexOf(rtMovie.year) !== -1) {
+							rtMovie.posterURL = tmdbConfig.images.base_url + tmdbConfig.images.poster_sizes[2] + dbMovie.poster_path;
+							rtMovie.overview = dbMovie.overview;
+							rtMovie.popularity = dbMovie.popularity;
+						}
+						innerCallback();
+					}, (err) => {
+						outerCallback(err);
+					});
+				}, (err) => {
+					if (!err) {
+						res.render('result-list.njk', { movies: results[0] });
+					} else {
+						res.render('oops.njk');
+						console.log(new Error(err));
+					}
+				});
+			}
+		});
+	});
+
+	app.get('/css/normalize.css', (req, res) => {
+		// res.redirect('https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css');
+		require('fs').createReadStream('./node_modules/normalize.css/normalize.css').pipe(res);
+	});
+
+	app.get('/:extension(css|js)/materialize.*', (req, res) => {
+		require('fs').createReadStream(path.join(__dirname, '/node_modules/materialize-css/bin/materialize.' + req.params.extension)).pipe(res);
+	});
+
+	// Turn on listening
+	app.listen(3000, function () {
+		console.log('Example app listening on port 3000!');
 	});
 });
 
-app.get('/css/normalize.css', (req, res) => {
-	res.redirect('https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css');
-	// require('fs').createReadStream('./node_modules/normalize.css/normalize.css').pipe(res);
-});
-
-app.get('/:extension(css|js)/materialize.*', (req, res) => {
-	require('fs').createReadStream(path.join(__dirname, '/node_modules/materialize-css/bin/materialize.' + req.params.extension)).pipe(res);
-});
-
-// Turn on listening
-app.listen(3000, function () {
-	console.log('Example app listening on port 3000!');
-});
+function getTMDbConfig () {
+	moviedb.configuration((err, config) => {
+		if (err) return null;
+		fs.writeFile(tmdbJSON, JSON.stringify(config, null, 2), (err) => {
+			if (err) console.log(new Error(err));
+			return config[0];
+		});
+	});
+}
--- test.js
+++
@@ -1,5 +0,0 @@
-var test;
-
-test = 'monkey';
-
-if (test) console.log('yep');
