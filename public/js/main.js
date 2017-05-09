35c270fc992ad5194eff613e5b136491bc45ad75
Gabriel Benson <me@gabe.xyz>
2017-05-09 16:27:44 -0400

    absolute url in client javascript

--- public/js/main.js
+++ public/js/main.js
@@ -3,14 +3,10 @@
 $(document).ready(function () {
 	$('input[name=search]').keyup(function (event) {
 		if (event.keyCode === 13) {
-			$.get('/film/' + $('input[name=search]').val(), function (data) {
+			$.get('https://gabe.xyz/rt_lite/film/' + $('input[name=search]').val(), function (data) {
 				$('main').html(data);
 			});
 			$('main').html('<div class="row"><p class="center s12 l6 offset-l3">Calling Rotten Tomatoes...</p></div>');
 		}
 	});
-});
-
-$(document).on('load', 'div.card-image img', function () {
-	console.log($(this).attr('title'));
 });
