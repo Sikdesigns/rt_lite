651a6949e84d95aab5a6214a4dad64defdb2df26
Gabriel Benson <me@gabe.xyz>
2017-05-11 16:28:33 -0400

    tweaked some things to have it work regardless of hosted location

--- index.js
+++ index.js
@@ -37,23 +37,30 @@ nunjucks.configure('views', {
 // Turn on public folder
 app.use(express.static('public'));
 
+// Load site configuration
+const config = JSON.parse(fs.readFileSync('config.json'));
+
 app.get('/', (req, res) => {
-	res.render('index.njk');
+	res.render('index.njk', config);
 });
 
 app.get('/film/:searchTerms', (req, res) => {
+	console.log(req.url);
 	const searchTerms = req.params.searchTerms.replace(/%20/g, ' ');
-	request(rtURL + req.url.substring(6), (err, response, html) => {
-		if (!err && response.statusCode === 200 && html.indexOf('Sorry, no results found') === -1) {
+	request.get(rtURL + req.url.substring(6), { timeout: 5000 }, (err, response, html) => {
+		if (err || response.statusCode !== 200) {
+			res.render('oops.njk', config);
+			console.log(new Error(err));
+		} else if (response.statusCode === 200 && html.indexOf('Sorry, no results found') === -1) {
 			const anchorString = searchTerms + '\', ';
 			var searchData = html.substring(html.indexOf(anchorString) + anchorString.length);
 			searchData = searchData.substring(0, searchData.indexOf(');'));
-			res.render('result-list.njk', { movies: JSON.parse(searchData).movies });
-		} else if (html.indexOf('Sorry, no results found') !== -1) {
-			res.render('nothing-found.njk');
+			res.render('result-list.njk', {
+				site: config.site,
+				movies: JSON.parse(searchData).movies
+			});
 		} else {
-			res.render('oops.njk');
-			console.log(new Error(err));
+			res.render('nothing-found.njk', config);
 		}
 	});
 });
--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@
 $(document).ready(function () {
 	$('input[name=search]').keyup(function (event) {
 		if (event.keyCode === 13) {
-			$.get('https://gabe.xyz/rt_lite/film/' + $('input[name=search]').val(), function (data) {
+			$.get(document.URL + (document.URL[document.URL.length - 1] === '/' ? '' : '/') + 'film/' + $('input[name=search]').val(), function (data) {
 				$('main').html(data);
 			});
 			$('main').html('<div class="row"><p class="center s12 l6 offset-l3">Calling Rotten Tomatoes...</p></div>');
