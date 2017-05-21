2e05a0dd4f782035c5e94db68f1d51d205a0ae17
Gabriel Benson <gabeotisbenson@gmail.com>
2017-05-21 13:45:41 -0400

    stop focusing search box when enter is hit

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,7 @@
 $(document).ready(function () {
 	$('input[name=search]').keyup(function (event) {
 		if (event.keyCode === 13) {
+			$(this).blur();
 			$.get(document.URL + (document.URL[document.URL.length - 1] === '/' ? '' : '/') + 'film/' + $('input[name=search]').val().replace(/\s/g, '-'), function (data) {
 				$('main').html(data);
 			});
